a56a76bde6c263154ce330417ca793cb8158062e
David Gilmore <david@poliana.com>
2014-05-03 22:25:00 -0500

    Passing tests on nested select sql generation from a view object. Still way more to do. Updated gitignore to exclude h2 generated swaps.

--- src/test/java/com/locke/olap/impl/JdbcSqlViewGeneratorUnitTest.java
+++ src/test/java/com/locke/olap/impl/JdbcSqlViewGeneratorUnitTest.java
@@ -6,8 +6,10 @@ import com.locke.olap.models.TableView;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * @author David Gilmore
@@ -30,7 +32,7 @@ public class JdbcSqlViewGeneratorUnitTest {
         selectView.setResource("test");
         selectView.setName("test_select_view");
 
-        List<String> columns = new ArrayList<>();
+        List<String> columns = new LinkedList<>();
 
         columns.add("name");
         columns.add("type");
@@ -46,5 +48,81 @@ public class JdbcSqlViewGeneratorUnitTest {
         selectView.setFrom(from);
 
         String query = this.jdbcSqlViewGenerator.createQuery(selectView);
+
+        assertEquals("select name, type, amount from test.test_table", query);
+    }
+
+    @Test
+    public void testCreateView__SimpleSelectFromTableNoTableResource() throws Exception {
+
+        SelectView selectView = new SelectView();
+
+        selectView.setResource("test");
+        selectView.setName("test_select_view");
+
+        List<String> columns = new LinkedList<>();
+
+        columns.add("name");
+        columns.add("type");
+        columns.add("amount");
+
+        selectView.setColumns(columns);
+
+        TableView from = new TableView();
+
+        from.setName("test_table");
+
+        selectView.setFrom(from);
+
+        String query = this.jdbcSqlViewGenerator.createQuery(selectView);
+
+        assertEquals("select name, type, amount from test_table", query);
+    }
+
+    @Test
+    public void testCreateView__NestedSelectWithFunctions() throws Exception {
+
+        SelectView from = new SelectView();
+
+        List<String> subColumns = new LinkedList<>();
+
+        subColumns.add("name");
+        subColumns.add("type");
+        subColumns.add("amount");
+
+        Map<String, String> functions = new HashMap<>();
+        functions.put("amount", "SUM");
+
+        List<Condition> where = new LinkedList<>();
+        where.add(new Condition("test_subquery", "name", "tom", "="));
+        where.add(new Condition("test_subquery", "type", "person", "="));
+
+        List<String> group = new LinkedList<>();
+        group.add("name");
+        group.add("type");
+
+        from.setResource("test");
+        from.setName("test_subquery");
+        from.setColumns(subColumns);
+        from.setFunctions(functions);
+        from.setGroup(group);
+        from.setWhere(where);
+
+
+        SelectView selectView = new SelectView();
+
+        List<String> columns = new ArrayList<>();
+        columns.add("name");
+        columns.add("type");
+        columns.add("total");
+
+        selectView.setResource("test");
+        selectView.setName("test_select_view");
+        selectView.setColumns(columns);
+        selectView.setFrom(from);
+
+        String query = this.jdbcSqlViewGenerator.createQuery(selectView);
+
+        assertNotNull(query);
     }
 }
